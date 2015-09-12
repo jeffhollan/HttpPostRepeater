4345836812be58aec4f9e7d11a90b1610f41c43b
Jeff Hollan <jeffhollan@outlook.com>
2015-09-11 21:16:25 -0700

    working

--- IntegrationMondayDemo/MainPage.xaml.cs
+++ IntegrationMondayDemo/MainPage.xaml.cs
@@ -15,7 +15,7 @@ namespace IntegrationMondayDemo
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private string[] locations = { "China", "India", "United States", "Indonesia", "Brazil", "Pakistan", "Nigeria", "Bangladesh", "Russia", "Japan", "Mexico", "Phillipines", "Vietnam", "Ethiopia", "Egypt", "United Kingdom", "Germany", "Turkey", "Iran", "France", "Italy", "Australia", "New Zealand" };
+        private string[] locations = { "China", "India", "United States", "Indonesia", "Brazil", "Pakistan", "Nigeria", "Bangladesh", "Russia", "Japan", "Mexico", "Phillipines", "Vietnam", "Ethiopia", "Egypt", "United Kingdom", "Germany", "Turkey", "Iran", "France", "Italy", "Australia", "New Zealand", "United States", "Canada", "Canada", "United Kingdom", "United Kingdom", "United Staes" };
         private string[] customers = { "Jeff", "Josh", "Stephen", "Priti", "Prashant", "Rachel", "Shyam", "Srivatsan", "Sameer", "Rajesh", "Jon", "Tony", "John", "Ping", "Lakshmi" };
         //Loading a Resources File that has the URL and AuthToken.  Doing this so I can store nice places like GitHub without compromising :)
         private string url;
@@ -34,7 +34,7 @@ namespace IntegrationMondayDemo
         private async void Start_Click(object sender, RoutedEventArgs e)
         {
             Random rnd = new Random();
-            for (int x = 0; x < 20; x++)
+            for (int x = 0; x < 500; x++)
             {
                 var upload = new Upload { name = "ManualTrigger", outputs = new Upload.Outputs { customer = customers[rnd.Next(0, customers.Length - 1)],  trackingId = Guid.NewGuid(), location = locations[rnd.Next(0, locations.Length - 1)] } };
                 Debug.WriteLine(JsonConvert.SerializeObject(upload));
